dabdd9b3793c8f0da777780416d5ef8a271a945b
vittorio rossetto <rossetto.vittorio2108@gmail.com>
2024-04-16 16:01:44 +0200

    Created test converter and started converter notebook

---
+++ chessConverterTest.py
@@ -0,0 +1,13 @@
+import chess 
+import pandas as pd
+
+games = pd.read_csv('./games.csv')
+
+board = chess.Board()
+san_mainline = "d4 Nc6 e4 e5 f4 f6 dxe5 fxe5"
+
+for move in san_mainline.split():
+    coordmove = str(board.parse_san(move))
+    board.push_san(move)
+    coord = [coordmove[:2], coordmove[2:]]
+    print(coord)
